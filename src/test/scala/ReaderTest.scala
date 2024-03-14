61d3580e8e860592bb161ec5284ccb83bb3ba924
Mikhail Druzhinin <druzhininmy@gmail.com>
2024-03-15 00:02:37 +0300

    update ReaderTest

--- src/test/scala/ReaderTest.scala
+++ src/test/scala/ReaderTest.scala
@@ -1,18 +1,25 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 import java.io.FileNotFoundException
-import scala.util.{Failure, Success}
+import scala.util.Success
 
 class ReaderTest extends AnyFunSuite {
   test("read existing file") {
     val inputFilePath: String = "src/main/resources/file.txt"
     val result = Reader(inputFilePath)
-    val expectedResult = Success("This is a text file. This is another sentence.")
+    val expectedResult = Success("This is a text file. This is another sentence. Это текстовый файл. Это второе предложение.")
+    assert(result == expectedResult)
+  }
+
+  test("read empty file") {
+    val inputFilePath: String = "src/test/resources/empty_file.txt"
+    val result = Reader(inputFilePath)
+    val expectedResult = Success("")
     assert(result == expectedResult)
   }
 
   test("read not existing file") {
-    val inputFilePath: String = "src/main/resources/file2.txt"
+    val inputFilePath: String = "src/test/resources/no_file.txt"
     val result = Reader(inputFilePath)
     assertThrows[FileNotFoundException](result.get)
   }
